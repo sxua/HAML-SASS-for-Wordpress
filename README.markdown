232d2687f67781336fb32811b492c3c27ba159e0
Olexandr Skrypnyk <ukraine.sax@gmail.com>
2011-03-28 16:09:47 +0300

    added some readme, plugin optimization and samples

---
+++ README.markdown
@@ -0,0 +1,25 @@
+HAML/SASS for Wordpress
+================================================================================
+This is a try to port an awesome markup language into Wordpress.
+
+How it works:
+--------------------------------------------------------------------------------
+Works good :) I'll told you soon how exactly.
+
+Sample:
+--------------------------------------------------------------------------------
+* TwentyTen theme in HAML/SASS
+> Working on it, some done parts:
+> layout.haml
+> 404.haml
+
+TODO:
+--------------------------------------------------------------------------------
+* partials support
+* multiple layouts
+
+Credits:
+--------------------------------------------------------------------------------
+Some ideas and code were borrowed from this projects:
+* [wp-haml] (https://github.com/dxw/wp-haml)
+* [sass-for-wordpress] (https://github.com/80beans/sass-for-wordpress)
--- haml-sass/haml-sass.php
+++ haml-sass/haml-sass.php
@@ -6,47 +6,67 @@ Author: Olexandr Skrypnyk
 Version: 1.0.2
 */
 
-define('COMPILED_TEMPLATES', WP_CONTENT_DIR . '/compiled-templates');
-register_activation_hook(__FILE__, 'haml_activate');
-register_deactivation_hook(__FILE__, 'haml_deactivate');
+register_activation_hook(__FILE__, 'haml_sass_activate');
+register_deactivation_hook(__FILE__, 'haml_sass_deactivate');
+add_filter('template_include', 'haml_template_include');
 
-function haml_activate() {
+define('COMPILED_TEMPLATES', WP_CONTENT_DIR . '/compiled-templates');
+
+function haml_sass_activate() {
+	$haml_version = exec('haml --version');
+	preg_match('/haml 3\.1.*/i',$haml_version,$match);
+	if ($haml_version === '') {
+		add_action('admin_notices','haml_not_installed');
+	} elseif (strtolower($match[1]) !== 'haml 3.1') {
+		add_action('admin_notices','haml_bad_version');
+	}
 	if (!file_exists(COMPILED_TEMPLATES) && !mkdir(COMPILED_TEMPLATES)) {
 		add_action('admin_notices', 'haml_dir_warning');
 	}
 }
 
-function haml_deactivate() {}
+function haml_sass_deactivate() {}
 
 function haml_dir_warning() {
-	echo "<div class='updated fade'><p>php-haml will currently work: you need to create <em>" . COMPILED_TEMPLATES . "</em> and make sure it's writeable by your webserver</p></div>";
+	echo '<div class="updated fade"><p>Wordpress can\'t access to directory <em>'.COMPILED_TEMPLATES.'</em>. Please make sure it\'s writeable by your webserver.</p></div>';
 }
 
-add_filter('template_include', 'haml_template_include');
+function haml_not_installed() {
+	echo '<div class="updated fade"><p>It seems, you have no Haml installed on your server. Install it with: <em>gem install haml --pre</em></p></div>';
+}
+
+function haml_bad_version() {
+	echo '<div class="updated fade"><p>In order to include <em><?php ?></em> tags in your HAML files, you need to install 3.1 version of Haml. <em>gem isntall haml --pre</em></p></div>';
+}
 
 function haml_fix_path($path) {
-	$path = explode('/',$path);
-	$size = sizeof($path);
-	$path[$size] = $path[$size - 1];
-	$path[$size - 1] = 'haml';
-	$path = join('/',$path);
-	return $path;
+	return preg_replace('/(.*)\/(\w+\.php)/i','$1/haml/$2',$path);
 }
 
 function haml_template_include($template) {
-	if (substr($template, -5) === '.haml') {
-		$haml_template = haml_fix_path($template);
+	$haml_template = (substr($template,-5) === '.haml') ? $template : str_replace('.php','.haml',$template);
+	haml_include(haml_fix_path(TEMPLATEPATH.'/layout.haml'),array('page' => haml_fix_path($haml_template)));
+}
+
+function do_camelcase($path) {
+	$path = str_replace('.haml','.php',$path);
+	$path_array = explode('-',$path);
+	if (sizeof($path_array) < 2) {
+		$camelcased_file_name = ucfirst(strtolower($path));
 	} else {
-		$haml_template = haml_fix_path(str_replace('.php', '.haml', $template));
+		$camelcased_file_name = '';
+		foreach ($path_array as $i => $part) {
+			$camelcased_file_name .= ($i === 0) ? strtolower($part) : ucfirst(strtolower($part));
+		}
 	}
-	haml_include(haml_fix_path(TEMPLATEPATH.'/layout.haml'),array('page' => $haml_template));
+	return COMPILED_TEMPLATES.'/'.strtolower(get_template()).'_'.$camelcased_file_name;
 }
 
 function haml_include($haml, $args = array()) {
-	$hphp = COMPILED_TEMPLATES.'/'.md5($haml).'.hphp';
-	if (!file_exists($hphp) || filemtime($hphp) < filemtime($haml)) {
-		@unlink($hphp);
-		exec('haml --no-escape-attrs --style ugly --format html5 '.escapeshellcmd($haml).' '.escapeshellcmd($hphp));
+	$cache = do_camelcase($haml);
+	if (!file_exists($cache) || filemtime($cache) < filemtime($haml)) {
+		@unlink($cache);
+		exec('haml --no-escape-attrs --style ugly --format html5 '.escapeshellcmd($haml).' '.escapeshellcmd($cache));
 	}
 	extract($args);
 	require($hphp);
@@ -56,48 +76,29 @@ function haml_body_class($class = '') {
 	echo join(' ',get_body_class($class));
 }
 
-function sass($filename) {
-	if (strpos($filename, '.css') || strpos($filename, '.sass')) {
-		$filename = str_replace(array('.css', '.sass'), '', $filename);
+function sass($file_name,$scss = false) {
+	$ext = ($scss) ? : '.scss' : '.sass';
+	if (strpos($file_name, '.css') || strpos($file_name, $ext)) {
+		$file_name = str_replace(array('.css', $ext), '', $file_name);
 	}
 	if (strpos($filename, '/')) {
-		$parts = explode('/', $filename);
-		$filename = $parts[count($parts)-1];
+		$parts = explode('/', $file_name);
+		$file_name = $parts[sizeof($parts)-1];
 	}
-	$sass_filename = TEMPLATEPATH.'/'.$filename.'.sass';
-	$css_filename = TEMPLATEPATH.'/'.$filename.'.css';
-	if (!file_exists($sass_filename)) {
-		sass_error($css_filename,'File '.$sass_filename.' does not exist.');
-	} else if (!file_exists($css_filename) || filemtime($css_filename) < filemtime($sass_filename)) {
-		@unlink($css_filename);
-		exec('sass '.escapeshellcmd($sass_filename).' '.escapeshellcmd($css_filename));
+	$sass = TEMPLATEPATH.'/'.$file_name.$ext;
+	$css = TEMPLATEPATH.'/'.$file_name.'.css';
+	if (!file_exists($sass)) {
+		sass_error($css,'File '.$sass.' does not exist.');
+	} else if (!file_exists($css) || filemtime($css) < filemtime($sass)) {
+		@unlink($css);
+		$run = ($scss) ? 'sass --scss ' : 'sass ';
+		exec($run.escapeshellcmd($sass).' '.escapeshellcmd($css));
 	}
-	
-	return get_bloginfo('template_directory') . '/' . $filename . '.css';
+	return get_bloginfo('template_directory') . '/' . $file_name . '.css';
 }
 
-function scss($filename) {
-	if (strpos($filename, '.css') || strpos($filename, '.scss')) {
-		$filename = str_replace(array('.css', '.scss'), '', $filename);
-	}
-	if (strpos($filename, '/')) {
-		$parts = explode('/', $filename);
-		$filename = $parts[count($parts)-1];
-	}
-	$sass_filename = TEMPLATEPATH . '/'. $filename . '.scss';
-	$css_filename = TEMPLATEPATH . '/'. $filename . '.css';
-	if (!file_exists($sass_filename)) {
-		sass_error($css_filename, 'File ' . $sass_filename . ' does not exist.');
-	} else if (!file_exists($css_filename) || filemtime($css_filename) < filemtime($sass_filename)) {
-		@unlink($css_filename);
-		exec('sass --scss '.escapeshellcmd($sass_filename).' '.escapeshellcmd($css_filename));
-	}
-	
-	return get_bloginfo('template_directory').'/'.$filename .'.css';
-}
-
-function sass_error($css_filename, $error) {
-	@unlink($css_filename);
-	file_put_contents($css_filename, 'body:before { white-space: pre; font-family: monospace; content: "Sass for Wordpress error: ' . str_replace('"','\"', $error) . '"; }');
+function sass_error($file,$error) {
+	@unlink($file);
+	file_put_contents($file, 'body:before { white-space: pre; font-family: monospace; content: "Sass error: '.str_replace('"','\"',$error).'";}');
 }
 ?>
